cf7703d0a0eea84524a2dfb03f8c1b05c850e0c2
hongquan16101995 <66354589+hongquan16101995@users.noreply.github.com>
2022-06-14 15:59:55 +0700

    staff manager

--- src/btthem/MenuStaff.java
+++ src/btthem/MenuStaff.java
@@ -1,73 +1,41 @@
 package btthem;
 
-import java.util.Scanner;
-
 public class MenuStaff {
-    private static int index = 0;
-    private static Staff[] arrayStaff;
-
     public static void main(String[] args) {
+        StaffManager staffManager = new StaffManager();
 
-        createArrayStaff();
-        System.out.println(arrayStaff.length);
+        //tạo ra 1 mảng chứa các Staff với length tương ứng
+        staffManager.createArrayStaff();
 
-        for (int i = 0; i < arrayStaff.length; i++) {
-            Staff objectStaff = createNewStaff();
-            addStaffInArrayStaff(objectStaff);
+        //lấy ra độ dài mảng StaffArray sau khi tạo
+        int staffArrayLength = staffManager.getArrayStaff().length;
+
+        //tạo hết đối tượng trong mảng
+        for (int i = 0; i < staffArrayLength; i++) {
+            Staff objectStaff = staffManager.createNewStaff();
+            staffManager.addStaffInArrayStaff(objectStaff);
         }
+
         //dùng để hiện thị các object tồn tại trong mảng trước khi xóa
-        for (Staff elementStaff : arrayStaff) {
-            System.out.println(elementStaff);
-        }
+        System.out.println("Các staff trước khi xóa ở vị trí index thứ 3: ");
+        staffManager.displayAllStaff();
 
-        deleteStaffByIndexInArray(3);
+        //xóa Staff tại vị trí nhập vào
+        staffManager.deleteStaffByIndexInArray();
 
         //dùng để hiện thị các object tồn tại trong mảng sau khi xóa
-        for (Staff elementStaff : arrayStaff) {
-            System.out.println(elementStaff);
-        }
-    }
+        System.out.println("Các staff sau khi xóa ở vị trí index thứ 3: ");
+        staffManager.displayAllStaff();
 
-    public static void deleteStaffByIndexInArray(int index) {
-        //tạo 1 mảng mới có số phần tử bằng số phần từ của mảng staff - 1
-        //đẩy các phần tử trong staff và mảng mới, ngoại trừ phần tử ở vị trí index
+        //hiển thị Staff tại vị trí nhập vào
+        System.out.println("Hiển thị Staff theo vị trí index: ");
+        staffManager.displayStaffByIndex();
 
-        //cần phân biệt 3 chỉ số thường dùng để thao tác với đối tượng trong mảng
-        //chỉ số 1: index - vị trí của phần tử trong mảng
-        //chỉ số 2: vị trí xuất hiện trên mảng
-        //chỉ số 3: id của đối tượng nếu có
-        Staff[] newStaff = new Staff[arrayStaff.length - 1];
-        for (int i = 0; i < arrayStaff.length - 1; i++){
-            if (i < index) {
-                newStaff[i] = arrayStaff[i];
-            } else {
-                newStaff[i] = arrayStaff[i + 1];
-            }
-        }
-        arrayStaff = newStaff;
-    }
+        //sửa thông tin của Staff tại vị trí nhập vào
+        staffManager.updateStaffByIndex();
 
-    public static void createArrayStaff() {
-        Scanner input = new Scanner(System.in);
-        System.out.println("Nhập vào độ dài bạn muốn của mảng chứa Staff: ");
-        int length = input.nextInt();
-        arrayStaff = new Staff[length];
-    }
-
-    public static Staff createNewStaff() {
-        Scanner input = new Scanner(System.in);
-        System.out.println("Nhập vào tên của Staff thứ " + (index + 1) + ": ");
-        String name = input.nextLine();
-        System.out.println("Nhập vào tuổi của Staff thứ " + (index + 1) + ": ");
-        int age = input.nextInt();
-        System.out.println("Nhập vào lương của Staff thứ " + (index + 1) + ": ");
-        double salary = input.nextDouble();
-        Staff newStaff = new Staff(name, age, salary);
-        return newStaff;
-    }
-
-    public static void addStaffInArrayStaff(Staff newStaff) {
-        arrayStaff[index] = newStaff;
-        index = index + 1;
+        //hiển thị lại sau cập nhật
+        System.out.println("Hiển thị tất cả Staff: ");
+        staffManager.displayAllStaff();
     }
 }
